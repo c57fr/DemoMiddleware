c16ea5ff6385a575081b5cc609a4ac07d5a8e3d6
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-13 06:20:26 +0100

    Test DatabaseTest avec commande et non méthode

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,6 +1,9 @@
 <?php namespace Gc7;
 
+use PDO;
 use PHPUnit\Framework\TestCase;
+
+require 'vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 class DatabaseTest extends TestCase {
   protected static $dbh;
@@ -8,29 +11,41 @@ class DatabaseTest extends TestCase {
   private static   $db_user = 'root';
   private static   $db_host = 'localhost';
   private static   $db_pass = '';
+  private          $rep;
 
   public static function setUpBeforeClass() {
     if (self::$dbh === null) {
-      $pdo = new \PDO("mysql:dbname=" . self::$db_name . ";host=" . self::$db_host, self::$db_user, self::$db_pass);
-      $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+      $pdo = new PDO("mysql:dbname=" . self::$db_name . ";host=" . self::$db_host, self::$db_user, self::$db_pass);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       self::$dbh = $pdo;
     }
 
     return self::$dbh;
   }
 
-  public function testUsers() {
-    $pdo = self::$dbh;
-    $sql = 'SELECT * FROM users';
-    $pdo = $pdo->query($sql);
-    $rep = $pdo->fetchAll();
-    var_dump($rep);
-    $this->assertArrayHasKey('username', $rep[0]);
-    $this->assertEquals('demo', $rep[0]['username']);
+  public function testUsernameField() {
+    $dbh = self::$dbh;
+    $sql = 'SELECT  * FROM users WHERE id=?';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([1]);
+    $rep = $stmt->fetch(PDO::FETCH_ASSOC);
+    assertTrue(1 == 1);
+    assertArrayHasKey('username', $rep);
+    return $rep;
   }
 
+  /**
+   * @depends testUsernameField
+   */
+  public function testUser1($rep) {
+    assertEquals('demo', $rep['username']);
+  }
 
   public static function tearDownAfterClass() {
     self::$dbh = null;
   }
 }
+
+// Qd ce fichier est dans tests, sinon adapter
+// Soit, déplacer ce ficher dans tests, soit changer commande
+// Console: phpunit tests/DatabaseTest --debug
