5f0cb19eeca88c4a25264cf240afc4ad3375b1c3
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-15 10:16:13 +0100

    Simple ReqAns (PSR15)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PSR15 - CSRF
 require 'vendor/autoload.php';
-use App\Dispatcher;
+//use App\Dispatcher;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\ServerRequest;
 
@@ -15,6 +15,31 @@ $request = ServerRequest::fromGlobals();
 $response = new Response();
 
 $url = (string)$request->getUri()->getPath();
+
+$response->getBody()->write('Oki');
+
+$response->getBody()->write('
+  <a href="/">Home</a> |
+  <a href="/blog/">Blog/</a> |
+  <a href="/blog">Blog</a> |
+  <a href="/contact">Contact</a>
+  <hr>');
+$response->getBody()->write('<h1>' . $url . '</h1><hr>');
+
+
+if ($url === '/')
+  $response->getBody()->write('Accueil');
+elseif ($url === '/blog')
+  $response->getBody()->write('Je suis sur le blog');
+elseif ($url === '/contact')
+  $response->getBody()->write('Me contacter');
+else {
+  $response->getBody()->write('Not found');
+  $response = $response->withStatus(404);
+}
+
+
+send($response);
 
 
 
